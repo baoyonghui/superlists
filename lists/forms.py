1234321dbe608a9d0f987d9e7800686b9e07572c
bao yonghui <baoyonghui@baodeMacBook-Pro.local>
2018-04-12 16:10:37 +0800

    add ExistingListItemForm to use validation in Form.

--- lists/forms.py
+++ lists/forms.py
@@ -1,15 +1,11 @@
 from django import forms
 from lists.models import Item
+from django.core.exceptions import ValidationError
 
+DUPLICATE_ITEM_ERROR = "You have already got this in your list"
 EMPTY_LIST_ERROR = "You can not have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
-    #text = forms.CharField(
-    #        widget=forms.fields.TextInput(attrs={
-    #            'placeholder': 'Enter a to-do item', 
-    #            'class': 'form-control input-lg', 
-    #        }),
-    #)
 
     class Meta:
         model = Item
@@ -26,4 +22,19 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
-        
+
+class ExistingListItemForm(ItemForm):
+
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
